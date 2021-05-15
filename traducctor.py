f1663827939092271ba71052744847ec0b675a85
Sebastian Giraldo Guarin <sebastiangg990407@gmail.com>
2021-05-14 21:41:35 -0500

    Se encapsulo en una funcion, donde se valida la existencia del archivo a traducir y además se exporta la traduccion a un archivo .txt

--- traducctor.py
+++ traducctor.py
@@ -1,5 +1,21 @@
 #Este programa es el pequeño uso de una librearia para traducir un Texto a español, básicamente lo use para traducir un archivo que es la descripción de un video de youtube.
+import os.path
 from deep_translator import GoogleTranslator
+
+
+#En la variable Path escribir la ruta completa de donde se encuentra el archivo.
+
+# La variable File tiene dos parametros, si queremos sobreescribir el archivo traduccion debemos poner A si queremos eliminarlo y escribir la nueva traduccion usaremos W.
+
 path = r'C:\Users\SEBASTIAN\Desktop\DESARROLLO\Traducctor_Py\text_to_translate.txt'
-translater = GoogleTranslator(source='en', target='es').translate_file(path)
-print(translater)
+def translate_file(path):
+    if os.path.isfile(path):
+        translater = GoogleTranslator(source='en', target='es').translate_file(path)
+        print(translater)
+        file = open("traduccion.txt", "w")
+        file.write(translater)
+    else:
+        print("Save the file in folder")
+
+translate_file(path)
+
